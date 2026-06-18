04095395d4fb1bb2e4370907b644015d01eb4675
shadowfox <arjunchilla532@gmail.com>
2026-06-18 11:53:03 +0530

    Delete task1_variables.py

--- task1_variables.py
+++
@@ -1,8 +0,0 @@
-pi = 22/7
-print("Value of pi:", pi)
-print("Data type:", type(pi))
-principal = 10000
-rate = 5
-time = 3
-simple_interest = (principal * rate * time) / 100
-print("Simple Interest:", simple_interest)

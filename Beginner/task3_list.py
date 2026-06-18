3332acc96eb804bdf5e5a15328d9f93bb4d4bde2
shadowfox <arjunchilla532@gmail.com>
2026-06-18 12:05:37 +0530

    Add files via upload

---
+++ Beginner/task3_list.py
@@ -0,0 +1,15 @@
+justice_league = ["Superman","Batman","Wonder Woman","Flash","Aquaman","Green Lantern"]
+print("Members:", len(justice_league))
+justice_league.extend(["Batgirl", "Nightwing"])
+print(justice_league)
+justice_league.remove("Wonder Woman")
+justice_league.insert(0, "Wonder Woman")
+print(justice_league)
+justice_league.remove("Green Lantern")
+aquaman_index = justice_league.index("Aquaman")
+justice_league.insert(aquaman_index + 1, "Green Lantern")
+print(justice_league)
+justice_league = ["Cyborg","Shazam","Hawkgirl","Martian Manhunter","Green Arrow"]
+justice_league.sort()
+print("Sorted List:", justice_league)
+print("Leader:", justice_league[0])
